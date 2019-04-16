b7c7aa32c6009c61d65c31fb9d70762ce9414feb
钦佳 王 <wangqinjia19960407@qq.com>
2019-04-16 17:22:34 +0800

    110ms

--- MarchingCube/MarchingCube.cpp
+++ MarchingCube/MarchingCube.cpp
@@ -32,9 +32,10 @@ Mesh MarchingCube::MarchingCubeCore(const float& target_value) const
 }
 
 void MarchingCube::MarchingCubeCore(Mesh& mesh, const float& target_value
-	, const  uint16& x_index, const  uint16& y_index, const  uint16& z_index
+	, const uint16& x_index, const uint16& y_index, const uint16& z_index
 	, const float& step_size, const float& scale)const
 {
+	const Vec3 subdivie_point = Vec3(x_index, y_index, z_index)* step_size;	//样本点
 	// 交点坐标，对于算法中的e1-e12。如果不插值的直接取中点的话，是可以预先计算好的
 	Vec3 isosurface_vertices_pos[12];
 	Vec3 isosurface_vertices_normal[12];
@@ -45,11 +46,6 @@ void MarchingCube::MarchingCubeCore(Mesh& mesh, const float& target_value
 	float a_offset;
 	int triangles_num = 0; // cube对应的三角形的数量
 
-	//样本点
-	const float x = x_index * step_size;
-	const float y = y_index * step_size;
-	const float z = z_index * step_size;
-	const Vec3 subdivie_point(x, y, z);
 
 	// 创建样本并查找cube顶点在表面内部或外部
 	for (int i = 0; i < 8; ++i)
@@ -99,12 +95,8 @@ void MarchingCube::MarchingCubeCore(Mesh& mesh, const float& target_value
 				a_offset = (target_value - cube_vertex_value[cube_edges_indices[i][0]]) / delta; //交点根据标量值进行插值
 			}
 			//插值出每条边上的交点
-			isosurface_vertices_pos[i].x = x + (cube_vertex_position[cube_edges_indices[i][0]].x
-				+ a_offset * cube_edge_direction[i].x) * scale;
-			isosurface_vertices_pos[i].y = y + (cube_vertex_position[cube_edges_indices[i][0]].y
-				+ a_offset * cube_edge_direction[i].y) * scale;
-			isosurface_vertices_pos[i].z = z + (cube_vertex_position[cube_edges_indices[i][0]].z
-				+ a_offset * cube_edge_direction[i].z) * scale;
+			isosurface_vertices_pos[i] = subdivie_point + (cube_vertex_position[cube_edges_indices[i][0]]
+				+ cube_edge_direction[i] * a_offset) * scale;
 			isosurface_vertices_normal[i] = CalVerticesNormal(x_index, y_index, z_index);
 		}
 	}
@@ -136,11 +128,11 @@ Vec3 MarchingCube::CalVerticesNormal(const uint16& x_index
 		(y_index >= 1 && y_index < cube_size_y) &&
 		(z_index >= 1 && z_index < cube_size_z))
 	{
-		int offset = -1;
+		const uint8 offset = 1;
 		// 中心差分
-		normal.x = GetDataUseXYZ(data, x_index + offset, y_index, z_index) - GetDataUseXYZ(data, x_index - offset, y_index, z_index);
-		normal.y = GetDataUseXYZ(data, x_index, y_index + offset, z_index) - GetDataUseXYZ(data, x_index, y_index - offset, z_index);
-		normal.z = GetDataUseXYZ(data, x_index, y_index, z_index + offset) - GetDataUseXYZ(data, x_index, y_index, z_index - offset);
+		normal.x = GetDataUseXYZ(data, x_index - offset, y_index, z_index) - GetDataUseXYZ(data, x_index + offset, y_index, z_index);
+		normal.y = GetDataUseXYZ(data, x_index, y_index - offset, z_index) - GetDataUseXYZ(data, x_index, y_index + offset, z_index);
+		normal.z = GetDataUseXYZ(data, x_index, y_index, z_index - offset) - GetDataUseXYZ(data, x_index, y_index, z_index + offset);
 		normal.normalize();
 	}
 	return normal;
@@ -149,5 +141,5 @@ Vec3 MarchingCube::CalVerticesNormal(const uint16& x_index
 inline float MarchingCube::GetDataUseXYZ(const float * data, const uint16& x
 	, const uint16& y, const uint16& z)const
 {
-	return data[x * cube_size_y * cube_size_z + y * cube_size_z + z];;
+	return data[x * cube_size_yz + y * cube_size_z + z];;
 }

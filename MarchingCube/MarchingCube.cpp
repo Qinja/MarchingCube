00387c32dbcf3d82d785a2dbbfe47ce63bf5c09d
钦佳 王 <wangqinjia19960407@qq.com>
2019-04-18 11:18:57 +0800

    细枝末节的规范化改进。

--- MarchingCube/MarchingCube.cpp
+++ MarchingCube/MarchingCube.cpp
@@ -2,7 +2,7 @@
 
 void MarchingCube::MarchingCubeCore(const float& target_value)
 {
-	for (int i = 0; i < max_threads; i++)
+	for (char i = 0; i < max_threads; i++)
 	{
 		threadVerticesCount[i] = 0;
 		threadWorkingNum[i] = 0;
@@ -11,13 +11,13 @@ void MarchingCube::MarchingCubeCore(const float& target_value)
 #pragma omp parallel for num_threads(max_threads) schedule (static)
 #endif // _DEBUG
 	// 循环遍历每个立方体，立方体数由细分数决定
-	for (int i = 1; i < cube_size_x - 1; ++i)
+	for (short i = 1; i < cube_size_x - 1; ++i)
 	{
 		const uint8 tIndex = omp_get_thread_num();
 		const uint16 i_mapping = max_threads * threadWorkingNum[tIndex] + tIndex + 1;
-		for (int j = 1; j < cube_size_y - 1; ++j)
+		for (uint16 j = 1; j < cube_size_y - 1; ++j)
 		{
-			for (int k = 1; k < cube_size_z - 1; ++k)
+			for (uint16 k = 1; k < cube_size_z - 1; ++k)
 			{
 				MarchingCubeCore(tIndex, target_value, i_mapping, j, k);
 			}
@@ -41,24 +41,21 @@ __forceinline void MarchingCube::MarchingCubeCore(const uint8& threadIndex
 	// 创建样本并查找cube顶点在表面内部或外部
 	for (uint8 i = 0; i < 8; ++i)
 	{
-		const uint8 x_offset = a2fVertexOffset[i][0];
-		const uint8 y_offset = a2fVertexOffset[i][1];
-		const uint8 z_offset = a2fVertexOffset[i][2];
-
-		const uint32 index = (x_index + x_offset) * cube_size_yz + (y_index + y_offset) * cube_size_z + z_index + z_offset;
+		const uint8* offset = a2fVertexOffset[i];
+		const uint32 index = (x_index + offset[0]) * cube_size_yz + (y_index + offset[1]) * cube_size_z + z_index + offset[2];
 		cube_vertex_value[i] = data[index];
-
-		if (cube_vertex_value[i] <= target_value)
+		if (cube_vertex_value[i] < target_value)
 		{
 			cube8_flag_index |= 1 << i; //位运算
 		}
 	}
 
-	uint16 edge_flag = aiCubeEdgeFlags[cube8_flag_index]; // 获取cube的交点flag
-	if (edge_flag == 0) //如果没有三角形需要画
+	if (cube8_flag_index == 0x00 || cube8_flag_index == 0xff) //如果没有三角形需要画	if (edge_flag == 0)
 	{
 		return;
 	}
+
+	uint16 edge_flag = aiCubeEdgeFlags[cube8_flag_index]; // 获取cube的交点flag
 
 	for (uint8 i = 0; i < 12; ++i)
 	{
@@ -66,49 +63,36 @@ __forceinline void MarchingCube::MarchingCubeCore(const uint8& threadIndex
 		{
 			// cube一条边的两个顶点的值之差
 			const float delta = cube_vertex_value[cube_edges_indices[i][1]] - cube_vertex_value[cube_edges_indices[i][0]];
-			float a_offset = 0.5f; //等值面与cube的边的交点，取cube边的中点
-			if (delta != 0.0)
-			{
-				a_offset = (target_value - cube_vertex_value[cube_edges_indices[i][0]]) / delta; //交点根据标量值进行插值
-			}
+			const float a_offset = (target_value - cube_vertex_value[cube_edges_indices[i][0]]) / delta; //交点根据标量值进行插值
 			//插值出每条边上的交点
 			isosurface_vertices_pos[i] = subdivie_point + (cube_vertex_position[cube_edges_indices[i][0]]
 				+ cube_edge_direction[i] * a_offset) * step_size;
-			isosurface_vertices_normal[i] = CalVerticesNormal(x_index, y_index, z_index);
+
+			const uint32 x1 = x_index * cube_size_yz + y_index * cube_size_z + z_index - cube_size_yz;
+			const uint32 x2 = x_index * cube_size_yz + y_index * cube_size_z + z_index + cube_size_yz;
+			const uint32 y1 = x_index * cube_size_yz + y_index * cube_size_z + z_index - cube_size_z;
+			const uint32 y2 = x_index * cube_size_yz + y_index * cube_size_z + z_index + cube_size_z;
+			const uint32 z1 = x_index * cube_size_yz + y_index * cube_size_z + z_index - 1;
+			const uint32 z2 = x_index * cube_size_yz + y_index * cube_size_z + z_index + 1;
+			isosurface_vertices_normal[i] = Vec3(data[x1] - data[x2], data[y1] - data[y2], data[z1] - data[z2]);
+			isosurface_vertices_normal[i].Normalize();
 		}
 	}
 
-	uint8 triangles_num = 0; // cube对应的三角形的数量
-	for (uint8 i = 0; i < 5; ++i)
+	const uint32 currentCount = threadVerticesCount[threadIndex];
+	uint8 triangles_num;
+	for (triangles_num = 0; triangles_num < 15; triangles_num += 3)
 	{
-		if (a2iTriangleConnectionTable[cube8_flag_index][3 * i] < 0) // <0 等价于 ==-1
+		if (a2iTriangleConnectionTable[cube8_flag_index][triangles_num] == 0xff) // 无三角形
 		{
 			break;
 		}
-		triangles_num++;
+		threadVertices[threadIndex][currentCount + triangles_num] = isosurface_vertices_pos[a2iTriangleConnectionTable[cube8_flag_index][triangles_num]];
+		threadVertices[threadIndex][currentCount + triangles_num + 1] = isosurface_vertices_pos[a2iTriangleConnectionTable[cube8_flag_index][triangles_num + 1]];
+		threadVertices[threadIndex][currentCount + triangles_num + 2] = isosurface_vertices_pos[a2iTriangleConnectionTable[cube8_flag_index][triangles_num + 2]];
+		threadNormals[threadIndex][currentCount + triangles_num] = isosurface_vertices_normal[a2iTriangleConnectionTable[cube8_flag_index][triangles_num]];
+		threadNormals[threadIndex][currentCount + triangles_num + 1] = isosurface_vertices_normal[a2iTriangleConnectionTable[cube8_flag_index][triangles_num + 1]];
+		threadNormals[threadIndex][currentCount + triangles_num + 2] = isosurface_vertices_normal[a2iTriangleConnectionTable[cube8_flag_index][triangles_num + 2]];
 	}
-
-	//cube对应三角形的边的索引数量
-	const uint8 triangles_edge_indices_num = triangles_num * 3;
-	const uint32 currentCount = threadVerticesCount[threadIndex];
-	for (uint8 i = 0; i < triangles_edge_indices_num; ++i)
-	{
-		threadVertices[threadIndex][currentCount + i] = isosurface_vertices_pos[a2iTriangleConnectionTable[cube8_flag_index][i]];
-		threadNormals[threadIndex][currentCount + i] = isosurface_vertices_normal[a2iTriangleConnectionTable[cube8_flag_index][i]];
-	}
-	threadVerticesCount[threadIndex] = currentCount + triangles_edge_indices_num;
-}
-
-__forceinline Vec3 MarchingCube::CalVerticesNormal(const uint16& x_index
-	, const uint16& y_index, const uint16& z_index)const
-{
-	const uint32 x1 = x_index * cube_size_yz + y_index * cube_size_z + z_index - cube_size_yz;
-	const uint32 x2 = x_index * cube_size_yz + y_index * cube_size_z + z_index + cube_size_yz;
-	const uint32 y1 = x_index * cube_size_yz + y_index * cube_size_z + z_index - cube_size_z;
-	const uint32 y2 = x_index * cube_size_yz + y_index * cube_size_z + z_index + cube_size_z;
-	const uint32 z1 = x_index * cube_size_yz + y_index * cube_size_z + z_index - 1;
-	const uint32 z2 = x_index * cube_size_yz + y_index * cube_size_z + z_index + 1;
-	Vec3 normal(data[x1] - data[x2], data[y1] - data[y2], data[z1] - data[z2]);
-	normal.Normalize();
-	return normal;
+	threadVerticesCount[threadIndex] = currentCount + triangles_num;
 }
